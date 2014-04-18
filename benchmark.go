9deef29a9c95fe741f87c24fa27e985e3f44d8f6
James A. Robinson <jim.robinson@stanford.edu>
2014-04-17 17:56:44 -0700

    added logic to allow for timeouts; modified format of the printed statistics

--- benchmark.go
+++ benchmark.go
@@ -11,11 +11,12 @@ import (
 // quickly the collection can iterate over its data
 // set while writes are being applied.
 type Benchmark struct {
-	id   string
-	c    Collection
-	mu   *sync.RWMutex
-	wg   *sync.WaitGroup
-	done chan bool
+	id    string
+	c     Collection
+	mu    *sync.RWMutex
+	wg    *sync.WaitGroup
+	guard *WatchDog
+	done  chan bool
 }
 
 // NewBenchmark returns a initialized Benchmark
@@ -23,9 +24,10 @@ type Benchmark struct {
 // id and database path.
 func NewBenchmark(id string, path string) (b *Benchmark, err error) {
 	b = &Benchmark{
-		id:   id,
-		wg:   &sync.WaitGroup{},
-		done: make(chan bool),
+		id:    id,
+		wg:    &sync.WaitGroup{},
+		guard: NewWatchDog(),
+		done:  make(chan bool),
 	}
 
 	switch id {
@@ -47,88 +49,162 @@ func NewBenchmark(id string, path string) (b *Benchmark, err error) {
 	return
 }
 
-// Wait blocks until the Run method has completed.
-func (b *Benchmark) Wait() {
+// Wait blocks until the Run method has completed,
+// and will return true if the benchmark was aborted.
+func (b *Benchmark) Wait() (aborted bool) {
 	b.wg.Wait()
+	return b.guard.IsAborted()
 }
 
 // Run launches two goroutines, one to read row sets
 // from ch, writing those rows to the collection, and another
 // to wake up every dur interval and poll the collection for
 // number of records and the time it took to iterate over
-// those records.
-func (b *Benchmark) Run(ch chan []*Row, dur time.Duration) {
+// those records.  If logevery is positive, the writer will
+// log a message every time approximately that many
+// rows have been written.
+func (b *Benchmark) Run(ch chan []*Row, logevery int, mw, dur, mr time.Duration) {
+	b.wg.Wait()
 	b.wg.Add(1)
-	go b.Writer(ch)
-	go b.Poll(dur)
+	go b.writer(ch, logevery, mw)
+	go b.poller(dur, mr)
 }
 
-// Writer reads rows from ch and writes them to
+// writer reads rows from ch and writes them to
 // the underlying Collection
-func (b *Benchmark) Writer(ch chan []*Row) {
-	n := int64(0)        // row set counter
-	ns := int64(0)       // elapsed time in nanoseconds
-	var t0, t1 time.Time // time between arrivals
-	for rows := range ch {
-		n, t1 = n+1, time.Now()
-		if n > 1 {
-			ns += t1.Sub(t0).Nanoseconds()
-		}
-		t0 = t1
+func (b *Benchmark) writer(ch chan []*Row, logevery int, timeout time.Duration) {
+	defer func() {
+		b.done <- true
+	}()
 
-		if b.mu != nil {
-			b.mu.Lock()
+	var sets_ttl int // row set counter
+	var rows_ttl int // row counter
+	var logged int   // rows_ttl we last logged
+
+	var a0, a1 time.Time        // time between arrivals
+	var arrival_t time.Duration // cumulative arrival times
+	var write_t time.Duration   // cumulative write time
+
+	for rows := range ch {
+		if b.guard.IsAborted() {
+			return
 		}
-		err := b.c.Set(rows)
-		if b.mu != nil {
-			b.mu.Unlock()
+
+		// a new row set has arrived, record the time
+		// and compute the elapsed time since the
+		// previous arrival, if applicable
+		a1 = time.Now()
+		sets_ttl += 1
+		rows_ttl += len(rows)
+
+		if sets_ttl > 1 {
+			arrival_t += time.Duration(a1.Sub(a0))
+		}
+
+		a0 = a1
+
+		// write the row set to the collection, using
+		// a goroutine to handle the case where the
+		// collection takes too long to return and we
+		// hit our WatchDog timeout.
+		var err error
+		ch := make(chan bool)
+		go func(write_t *time.Duration, err *error) {
+
+			if b.mu != nil {
+				b.mu.Lock()
+			}
+
+			w0 := time.Now()
+			*err = b.c.Set(rows)
+			w1 := time.Now()
+
+			// add to the elapsed time counter
+			*write_t += time.Duration(w1.Sub(w0))
+
+			if b.mu != nil {
+				b.mu.Unlock()
+			}
+
+			ch <- true
+		}(&write_t, &err)
+
+		select {
+		case _ = <-ch:
+			// success
+		case _ = <-b.guard.Timer(mw):
+			b.guard.Abort(fmt.Sprintf("write timeout reached: %s", mw))
+			return
 		}
 
 		if err != nil {
 			log.Println(err)
 			return
 		}
+
+		if logevery > 0 && rows_ttl-logged > logevery {
+			log.Printf("%d rows written\n", rows_ttl)
+			logged = rows_ttl
+		}
 	}
 
-	b.done <- true
-	if n > 0 {
+	if sets_ttl > 0 {
 		log.Printf("%d row sets arrived at an average inter-arrival rate of %s",
-			n, time.Duration(ns/n))
+			sets_ttl, time.Duration(arrival_t.Nanoseconds()/int64(sets_ttl)))
+
+		log.Printf("%s: %d write ops in %s: %d ns/op\n",
+			b.id, rows_ttl, write_t, write_t.Nanoseconds()/int64(rows_ttl))
 	}
 }
 
-// Poll wakes up every dur duration and polls the
-// underlying Collection Timer.
-func (b *Benchmark) Poll(dur time.Duration) {
+// poller wakes up every cycle duration and polls the
+// underlying Collection Timer.  If a read takes more
+// than timeout, abort.
+func (b *Benchmark) poller(cycle, timeout time.Duration) {
 	defer b.wg.Done()
+
+	var n int                // total rows read
+	var read_t time.Duration // cumulative read time
+
 	for {
+		if b.guard.IsAborted() {
+			return
+		}
+
 		select {
 		case _ = <-b.done:
-			if b.mu != nil {
-				b.mu.RLock()
-			}
-			n, t := b.c.Timing()
-			if b.mu != nil {
-				b.mu.RUnlock()
-			}
-			ms := t.Nanoseconds() / 1e6
-			opsms := int64(n) / ms
-			log.Printf("%s: %d ops in %d ms: %d ops/ms\n",
-				b.id, n, ms, opsms)
+			log.Printf("%s: %d read ops in %s: %d ns/op\n",
+				b.id, n, read_t, (read_t.Nanoseconds() / int64(n)))
 			return
 		default:
-			time.Sleep(dur)
-			if b.mu != nil {
-				b.mu.RLock()
+			time.Sleep(cycle)
+
+			ch := make(chan bool)
+			go func(n *int, read_t *time.Duration) {
+				log.Println("polling collection")
+
+				if b.mu != nil {
+					b.mu.RLock()
+				}
+
+				i, t := b.c.Timing()
+
+				*n += i
+				*read_t += t
+
+				if b.mu != nil {
+					b.mu.RUnlock()
+				}
+				ch <- true
+			}(&n, &read_t)
+
+			select {
+			case _ = <-ch:
+				// success
+			case _ = <-b.guard.Timer(mr):
+				b.guard.Abort(fmt.Sprintf("read timeout reached: %s", mr))
+				return
 			}
-			n, t := b.c.Timing()
-			if b.mu != nil {
-				b.mu.RUnlock()
-			}
-			ms := t.Nanoseconds() / 1e6
-			opsms := int64(n) / ms
-			log.Printf("%s: %d ops in %d ms: %d ops/ms\n",
-				b.id, n, ms, opsms)
 		}
 	}
 }
---
+++ watchdog.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+// WatchDog provides a mechanism to time out on
+// a benchmark if too much time has elapsed since
+// the most recent operation.
+type WatchDog struct {
+	sync.Mutex
+
+	// aborted is set to true when a
+	// timeout has been reached
+	aborted bool
+}
+
+func NewWatchDog() *WatchDog {
+	guard := &WatchDog{
+		aborted: false,
+	}
+	return guard
+}
+
+// Timer returns a channel that will return
+// true if the max time has been reached
+func (guard *WatchDog) Timer(dur time.Duration) (ch <-chan time.Time) {
+	return time.NewTimer(dur).C
+}
+
+// Abort sets the watchdog to the aborted state
+func (guard *WatchDog) Abort(msg string) {
+	guard.Lock()
+	guard.aborted = true
+	guard.Unlock()
+	log.Println(msg)
+}
+
+// IsAborted returns true if the watchdog
+// has been triggered
+func (guard *WatchDog) IsAborted() bool {
+	guard.Lock()
+	defer guard.Unlock()
+	return guard.aborted
+}
